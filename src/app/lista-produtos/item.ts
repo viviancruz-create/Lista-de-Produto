ab7b57a53830f329526aad41972ba53303fb49d4
vivian suenny <vivian.cruz@aluno.senai.br>
2026-07-30 21:00:01 -0300

    terminando as funcoes

---
+++ src/app/lista-produtos/item.ts
@@ -0,0 +1,13 @@
+export class Item{
+    idProduto?: number
+    descricaoProduto?: string
+    valorUnitario?: number
+    statusItem : boolean = false
+
+    /*
+    constructor( descricaoProdutos: string, valorUnitario: number) {
+        this.descricaoProduto = descricaoProduto
+        this.valorUnitario = valorUnitario
+        
+    }*/
+}
--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Item } from './item';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  descricao_produto? : string;
+  valor_unitario? : number;
+  listaItens: Item[] = []
+
+  addItem(){
+    //Intancia da classe item passando os parâmetros do construtor
+    //let item = new Item(this.descricao_produto, this.valor_unitario)
+   //Instãncia da classe item ( criando o objeto item)
+
+    let item = new Item();
+    item.idProduto = this.listaItens.length + 1
+    item.descricaoProduto = this.descricao_produto;
+    item.valorUnitario = this.valor_unitario;
+
+    //Adicionando objeto item ao array listaItens
+    this.listaItens.push(item)
+
+    //Limpando as propriedade da classe
+    this.descricao_produto = ''
+    this.valor_unitario = 0.0
+
+  }
+}
